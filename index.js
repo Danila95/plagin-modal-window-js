ecc2b96649719f56c3f8d3e7ea01e0be017eaf98
ddb4 <ddb4@tpu.ru>
2021-04-29 14:36:06 +0700

    оживил функционал модального окна
    -реализовал параметр title
    -реализовал параметр closable
    -реализовал параметр content
    -реализовал параметр width
    -реализовал параметр footerButtons
    -реализовал функцию destroy()
    -реализовал функцию setContent()

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'title modal window',
+    closable: true,
+    content: `
+    <p>Lorem ipsum dolor sit.</p>
+    <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '400px',
+    footerButtons: [ //массив в котором описываются параметры кнопок, которые будут находиться в footer
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primary btn clicked');
+            modal.close();
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clicked');
+            modal.close();
+        }}
+    ]
+})
